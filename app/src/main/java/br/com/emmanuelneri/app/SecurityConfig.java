1dcb0dbe2c77d0b2842c4bc1e85549c4b02533f2
Emmanuel Neri <emmanuelnerisouza@gmail.com>
2018-12-18 22:30:31 -0200

    simplifying security configuration

--- app/src/main/java/br/com/emmanuelneri/app/SecurityConfig.java
+++ app/src/main/java/br/com/emmanuelneri/app/SecurityConfig.java
@@ -1,36 +1,20 @@
 package br.com.emmanuelneri.app;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Value("${spring.security.user.name}")
-    private String user;
-
-    @Value("${spring.security.user.password}")
-    private String password;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .authenticated()
+        http.csrf().disable()
+                .authorizeRequests()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
+                .antMatchers("/**").authenticated()
                 .and()
                 .httpBasic();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser(user)
-                .password("{noop}" + password)
-                .roles("ADMIN");
     }
 }
